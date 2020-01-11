63d69fbd9bab48d9089444e5a8e9d09e352a0dcc
Jorge Gomez <jjgomez@ucm.es>
2020-01-11 20:04:09 +0100

    trying starting a web

--- template/java-maven-spring/function/src/main/java/function/Handler.java
+++ template/java-maven-spring/function/src/main/java/function/Handler.java
@@ -1,13 +1,16 @@
 package function;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/")
 class Handler {
 
 	 @RequestMapping("/")
-    public String  handle() {
-        return " Hello World from Kotlin Spring Boot!\n".trim();
+    public ResponseEntity<String>  handle() {
+        return new ResponseEntity<>(" Hello World from Kotlin Spring Boot!\n".trim(), HttpStatus.OK);
     }
 
 }
